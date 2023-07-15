ad526a10b4d9d8cfccef7e18bb1554cc1518b208
Sage Choi <coolcuds@gmail.com>
2023-07-15 11:04:59 -0700

    Set up quiz body contents

---
+++ src/components/QuizContents.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './QuizContents.css';
+
+const QuizContents = () => {
+  const navClass = 'nav-links';
+
+  return (
+    <div className='quiz-question__body'>
+      
+    </div>
+  );
+};
+
+export default QuizContents;
