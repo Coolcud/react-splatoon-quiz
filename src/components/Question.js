639198578dec7e471190742354558d73ab3e84c3
Sage Choi <coolcuds@gmail.com>
2023-07-15 12:19:24 -0700

    Add image tag

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './Question.css';
 import PropTypes from 'prop-types';
 
-const Question = ({ id, question, options, answer }) => {
-
+const Question = ({ id, image, question, options, answer }) => {
+  return (
+    <div>
+      <img></img>
+    </div>
+  );
 };
 
 // Typecheck each prop
 Question.propTypes = {
   id: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
   question: PropTypes.string.isRequired,
   options: PropTypes.objectOf(PropTypes.arrayOf(PropTypes.string)),
   answer: PropTypes.string.isRequired
