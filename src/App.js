28ac0c7f4f8c83ef14b45205291523bd26595dc3
Sage Choi <coolcuds@gmail.com>
2023-07-15 11:44:27 -0700

    Pass quizData to QuestionList and modify import of quizData

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import QuestionList from './components/QuestionList';
-import quizData from '../data/questions.json';
+import quizData from './data/questions.json';
 
 function App() {
   return (
-    <main>
-      <header className='page-header'>Splatoon Trivia!</header>
-      <section className='page-quiz__body'>
-        <QuestionList></QuestionList>
-      </section>
-    </main>
+    <div id='App'>
+      <main>
+        <header className='page-header'>Splatoon Trivia!</header>
+        <section className='page-quiz__body'>
+          <QuestionList questions={quizData}/>
+        </section>
+      </main>
+    </div>
   );
-}
+};
 
 export default App;
