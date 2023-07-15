6a4f893925e68181ba0b29885208a154adfb842a
Sage Choi <coolcuds@gmail.com>
2023-07-15 11:04:39 -0700

    Replace default content with quiz content

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import QuizContents from './components/QuizContents';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <header className='page-header'>Splatoon Trivia!</header>
+      <section className='page-quiz__body'>
+        <QuizContents></QuizContents>
+      </section>
+    </main>
   );
 }
 
