9a6f7f56c5739fa3fca7f9610379da4c438e43df
Sage Choi <coolcuds@gmail.com>
2023-07-15 12:18:40 -0700

    Add header image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import QuestionList from './components/QuestionList';
 import quizData from './data/questions.json';
+import s3GenericImg from './images/s3-generic.jpg';
 
 function App() {
   return (
     <div id='App'>
       <main>
-        <header className='page-header'>Splatoon Trivia!</header>
+        <header className='page-header'>
+          Splatoon Trivia!
+          <img alt='inklings fighting' src={s3GenericImg}/>
+        </header>
         <section className='page-quiz__body'>
           <QuestionList questions={quizData}/>
         </section>
