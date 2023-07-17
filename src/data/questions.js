f8a2514d40bbd8bffdb20194c067313029c3662a
Sage Choi <coolcuds@gmail.com>
2023-07-17 01:08:21 -0700

    Remove id from each question

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,23 +1,22 @@
-export const quiz = [
-  {
-    id: 1,
-    image: "s3-deep-cut-logo.jpg",
-    question: "Which of the following characters is NOT part of Deep Cut?",
-    options: ["Big Man", "Judd", "Shiver", "Frye"],
-    answer: "b"
-  },
-  {
-    id: 2,
-    image: "s3-splatfest-tricolor.jpg",
-    question: "How many splatfests have there been in North America?",
-    options: ["57", "51", "50", "55"],
-    answer: "d"
-  },
-  {
-    id: 3,
-    image: "s2-ability-chunks.jpg",
-    question: "In which game was the ability 'Ink Resistance Up' introduced?",
-    options: ["Splatoon 1", "Splatoon 2", "Splatoon 3"],
-    answer: "a"
-  }
-];
+export const quiz = {
+  questions: [
+    {
+      question: "Which of the following characters is NOT part of Deep Cut?",
+      options: ["Big Man", "Judd", "Shiver", "Frye"],
+      correctAnswer: "b",
+      image: "s3-deep-cut-logo.jpg"
+    },
+    {
+      question: "How many splatfests have there been in North America?",
+      options: ["57", "51", "50", "55"],
+      correctAnswer: "d",
+      image: "s3-splatfest-tricolor.jpg"
+    },
+    {
+      question: "In which game was the ability 'Ink Resistance Up' introduced?",
+      options: ["Splatoon 1", "Splatoon 2", "Splatoon 3"],
+      correctAnswer: "a",
+      image: "s2-ability-chunks.jpg"
+    }
+  ]
+};
