07ea46898985e9f24c38f1f084dfe7a6a49fd4ff
Sage Choi <coolcuds@gmail.com>
2023-07-17 09:50:07 -0700

    Change correctAnswer to correct string answer

--- src/data/questions.js
+++ src/data/questions.js
@@ -3,19 +3,19 @@ export const quiz = {
     {
       question: "Which of the following characters is NOT part of Deep Cut?",
       options: ["Big Man", "Judd", "Shiver", "Frye"],
-      correctAnswer: "b",
+      correctAnswer: "Judd",
       image: "s3-deep-cut-logo.jpg"
     },
     {
       question: "How many splatfests have there been in North America?",
       options: ["57", "51", "50", "55"],
-      correctAnswer: "d",
+      correctAnswer: "55",
       image: "s3-splatfest-tricolor.jpg"
     },
     {
       question: "In which game was the ability 'Ink Resistance Up' introduced?",
       options: ["Splatoon 1", "Splatoon 2", "Splatoon 3"],
-      correctAnswer: "a",
+      correctAnswer: "Splatoon 1",
       image: "s2-ability-chunks.jpg"
     }
   ]
