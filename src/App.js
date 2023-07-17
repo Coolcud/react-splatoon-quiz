035aa7879fde0cd5c18db7bdae1438c71f9a94b5
Sage Choi <coolcuds@gmail.com>
2023-07-17 01:10:59 -0700

    Remove header and add Quiz component to main

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import './App.css';
-import QuestionList from './components/QuestionList';
-import quizData from './data/questions.json';
-import s3GenericImg from './images/s3-generic.jpg';
+import Quiz from './components/Quiz';
 
 function App() {
   return (
     <div id='App'>
       <main>
-        <header className='page-header'>
-          Splatoon Trivia!
-          <img alt='inklings fighting' src={s3GenericImg}/>
-        </header>
         <section className='page-quiz__body'>
-          <QuestionList questions={quizData}/>
+          <Quiz></Quiz>
         </section>
       </main>
     </div>
